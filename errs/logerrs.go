5e1fd72e915406a4245e77dd24979dcd14430954
liuzehao <1437070389@qq.com>
2021-10-12 22:10:48 +0800

    split init func and made it more flexible

--- errs/logerrs.go
+++ errs/logerrs.go
@@ -1,22 +1,49 @@
 package errs
 
 import (
+	"context"
+	"google.golang.org/grpc"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 	"log"
-	"pasteProject/base"
+	basepb "pasteProject/base/api/gen/go"
+	"pasteProject/base/base"
 	"pasteProject/models"
 	"time"
 )
 
 var (
-	DB *gorm.DB
+	DB     *gorm.DB // 数据库实例
+	DSN    string   // 当前DSN
+	conn   *grpc.ClientConn
+	client basepb.BaseServiceClient
 )
 
-// 此处初始化数据库
-func init() {
+// 启动客户端获取mysql配置信息
+func loadMysql() string {
 	var err error
-	DB, err = gorm.Open(mysql.Open(base.Load()), &gorm.Config{})
+	conn, err = grpc.Dial("localhost:8084", grpc.WithInsecure())
+	if err != nil {
+		log.Fatalf("load: %v", err)
+	}
+	client = basepb.NewBaseServiceClient(conn)
+	res, err := client.GetEnv(context.Background(), &basepb.BaseRequest{Code: 1})
+	if err != nil {
+		log.Fatalf("load get env: %v", err)
+	}
+	if DSN == "" {
+		base.PreSig = res.Sig
+		base.NowSig = res.Sig
+	}
+	//fmt.Println(res.Mysql.Dsn)
+	return res.Mysql.Dsn
+}
+
+// InitMysql 初始化数据库
+func InitMysql(DSN string) {
+	//DSN = loadMysql()
+	var err error
+	DB, err = gorm.Open(mysql.Open(DSN), &gorm.Config{})
 	if err != nil {
 		log.Fatalf("init mysql falied: %v", err)
 	}
@@ -46,7 +73,24 @@ func init() {
 			Where:   "loggers.go",
 		})
 	}
+}
 
+// 此处初始化数据库
+func init() {
+	InitMysql(loadMysql())
+}
+
+func Refresh() {
+	env, err := client.GetEnv(context.Background(), &basepb.BaseRequest{Code: 1})
+	if err != nil && base.PreSig == 0 {
+		log.Fatalf("get env failed: %v", err)
+	} else if err != nil && base.PreSig != 0 {
+		return
+	}
+	if base.PreSig != env.Sig {
+		InitMysql(loadMysql())
+		base.PreSig = env.Sig
+	}
 }
 
 func GetDB() *gorm.DB {
