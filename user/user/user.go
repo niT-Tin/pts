05ce21a538a68ad1c75cc3595290f571fef1fe40
liuzehao <1437070389@qq.com>
2021-10-12 22:13:13 +0800

    add the refresh env steps

--- user/user/user.go
+++ user/user/user.go
@@ -2,15 +2,34 @@ package user
 
 import (
 	"context"
+	"gorm.io/gorm"
 	"net/http"
+	"pasteProject/errs"
+	"pasteProject/repositories"
 	userpb "pasteProject/user/api/gen/go"
 )
+
+var (
+	user       repositories.IUserRepo
+	errHandler errs.IErr
+)
+
+func Init(db *gorm.DB) {
+	user = repositories.NewUserRepo(db)
+	errHandler = errs.NewErrs(db)
+}
+
+func init() {
+	Init(errs.GetDB())
+}
 
 type Service struct {
 	userpb.UnimplementedUserServiceServer
 }
 
 func (s *Service) Login(ctx context.Context, req *userpb.UserRequest) (resp *userpb.UserResponse, err error) {
+	errs.Refresh()
+	Init(errs.GetDB())
 	return &userpb.UserResponse{
 		StatusCode: http.StatusOK,
 		Token:      "This is a token " + req.Username,
